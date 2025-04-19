b3c0cf14209391600656a2286269194cb322c9a7
oricardoz <ricardoapzaghetto@gmail.com>
2025-04-19 19:47:42 -0300

    Iniciando a função de buscar todos

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,24 +1,68 @@
 package middleware
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 	"net/http"
 	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+func init() {
+	loadTheEnv()
+	createDBInstance()
+}
+
+func loadTheEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func createDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collName := os.Getenv("DB_COLLECTION_NAME")
+
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOption)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("connected to MongoDB")
+
+	client.Database(dbName).Collection(collName)
+
+	fmt.Print("Collection created")
+}
 
 var collection *mongo.Collection
 
-func GetAllTask(w http.ResponseWriter, r *http.Request) {}
+func GetAllTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	payload := getAllTask()
+	json.NewEncoder(w).Encode(payload)
+}
 
-func CreateTask(w http.ResponseWriter, r *http.Request) {}
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func DeleteTask(w http.ResponseWriter, r *http.Request) {}
 
